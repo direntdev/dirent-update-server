7b55809819049e60fc7fdae7038cb0928eede231
Mateusz Migas <mig@leapwork.com>
2023-05-31 01:19:19 +0200

    works

--- api/check.ts
+++ api/check.ts
@@ -21,9 +21,12 @@ const githubApi = {
         }
       );
       const links = result.data[0].assets.map((asset) => ({
+        id: asset.id,
         name: asset.name,
         url: asset.browser_download_url,
       }));
+      console.log("links", links);
+
       return result.data[0].tag_name;
     } catch (error) {
       console.log("error", error);
@@ -36,22 +39,6 @@ export default async function handler(
   response: VercelResponse
 ) {
   const tag = await githubApi.getLatestReleaseTag();
-  const redirect = "manual";
-  const headers = { Accept: "application/octet-stream" };
-  const options = { headers, redirect };
-  // const rawUrl = `https://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@github.com/direntdev/dirent-app/releases/download/v0.0.3/dirent-0.0.3-full.nupkg`;
-  // const finalUrl = rawUrl.replace(
-  //   "https://api.github.com/",
-  //   `https://${env.GITHUB_TOKEN}@api.github.com/`
-  // );
-
-  // console.log("finalUrl", rawUrl);
-
-  // fetch(rawUrl, options as any).then((assetRes: any) => {
-  //   response.setHeader("Location", assetRes.headers.get("Location"));
-  //   response.send(302);
-  // });
-
   response.status(200).json({
     body: tag,
   });
--- api/download.ts
+++ api/download.ts
@@ -1,45 +1,40 @@
+import { Octokit, App } from "octokit";
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import https from "https";
-import axios from "axios";
-import fs from "fs";
+
 const env = {
   GITHUB_TOKEN: process.env.GITHUB_TOKEN,
 };
 
-//method to download github release asset
+const octokit = new Octokit({ auth: env.GITHUB_TOKEN });
+
+const streamAsset = async (assetId, res) => {
+  const result = await octokit.request(
+    "GET /repos/direntdev/dirent-app/releases/assets/{asset_id}",
+    {
+      owner: "OWNER",
+      repo: "REPO",
+      asset_id: assetId,
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28",
+        Accept: "application/octet-stream",
+      },
+    }
+  );
+  const downloadUrl = result.url;
+
+  https
+    .get(downloadUrl, (response) => {
+      response.pipe(res);
+    })
+    .on("error", (err) => {
+      console.error("Error downloading file:", err);
+    });
+};
 
 export default function handler(
   request: VercelRequest,
   response: VercelResponse
 ) {
-  axios({
-    method: "get",
-    url: "https://github.com/direntdev/dirent-app/releases/download/v0.0.5/RELEASES",
-    // responseType: "stream",
-    headers: {
-      Authorization: `bearer ${env.GITHUB_TOKEN}`,
-      Accept: "application/octet-stream",
-    },
-  }).then(function (res) {
-    res.data.pipe(response, { end: true });
-    // res.data.pipe(fs.createWriteStream("ada_lovelace.jpg"));
-  });
-
-  // const connector = https.get(
-  //   "https://github.com/direntdev/dirent-app/releases/download/v0.0.5/RELEASES",
-  //   {
-  //     headers: {
-  //       Authorization: `bearer ${env.GITHUB_TOKEN}`,
-  //       Accept: "application/octet-stream",
-  //     },
-  //   },
-  //   (res) => {
-  //     console.log("downloaded2", res.headers["content-type"], res.statusCode);
-  //     response.setHeader("content-type", res.headers["content-type"]!);
-  //     response.statusCode = res.statusCode ?? 200;
-  //     return res.pipe(response, { end: true });
-  //   }
-  // );
-
-  // request.pipe(connector, { end: true });
+  streamAsset(92447553, response);
 }
